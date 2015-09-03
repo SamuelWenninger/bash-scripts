d3eb6ca6451e4df880aa9aef1faa35313b5be610
SamuelWenninger <wennis@rpi.edu>
2015-09-02 23:58:27 -0400

    Added incrementation to names with collisions both previously existing and newly created

--- rename-cs.bash
+++ rename-cs.bash
@@ -1,8 +1,9 @@
-#! /bin/bash
+#! /usr/local/bin/bash
+#CAUTION!!! REQUIRES BASH 4.0 OR GREATER!!!
 
 #Created by: Samuel Wenninger
 #Created on: 07/06/2015
-#Last modified: 07/08/2015
+#Last modified: 09/02/2015
 #About: This bash script renames all of the files in the current working
 ##directory. To force all files in the current working directory to be renamed
 ##without prompting for confirmation, use "-f". To recursively rename all of
@@ -12,6 +13,10 @@
 function rename() {
     #Number of changed filenames
     NumChanged=0
+    #Associative array (-A) used to ensure that two files do not get renamed to
+    #the same thing.
+    declare -A NameList
+    declare -A OtherList
     #If no flag, prompt the user for confirmation before renaming files
     if [ $FORCE == 0 ]; then
         read -p "Are you sure you want to rename all files in the current 
@@ -26,6 +31,10 @@ function rename() {
         #cd into the current working directory so that all of the files in said
         #directory can be looped over and renamed
         cd "$PWD"
+        for i in *
+        do
+            OtherList[$i]=0
+        done
         #Loop over each file in the current directory
         for i in *
         do
@@ -44,10 +53,39 @@ function rename() {
             #Don't bother to rename a file that is already in the correct form. 
             #Also, only count files that are actually renamed using NumChanged.
             if [ "$i" != $TEMP ]; then
+                #If multiple files are renamed to the same name, append the
+                #appropriate number to the name. Bash arrays do not allow for
+                #changing the value after it has been set so "unset" must be
+                #used.
+                if [[ ${NameList[$TEMP]} != '' ]]; then
+                    #Separate out the name and the extension using regex
+                    NAME="$(echo "$TEMP" | perl -pe 's/\..*//g')"
+                    EXTENSION="$(echo "$TEMP" | perl -pe 's/^[^\.]+//g')"
+                    VAL=${NameList[$TEMP]}
+                    while : 
+                    do
+                        VAL=$(($VAL+1))
+                        TEST="$NAME-$VAL$EXTENSION"
+                        #Check for conflicts between already renamed files and
+                        #files already in the proper format
+                        if [[ ${NameList[$TEST]} == '' ]] && 
+                                           [[ ${OtherList[$TEST]} == '' ]]; then
+                        unset NameList[$TEMP]
+                        NameList[$TEMP]=$VAL
+                        TEMP="$NAME-$VAL$EXTENSION"
+                            break
+                        fi
+                    done
+                fi
                 mv "$i" "$TEMP"    
                 #Show the user what was changed
                 echo; echo ""$i" ===> "$TEMP""
                 let ++NumChanged
+            fi
+            #Add new properly formatted names to the associative array of
+            #renamed files.
+            if [[ ${NameList[$TEMP]} == '' ]]; then
+                NameList[$TEMP]=0
             fi
         done
         echo
